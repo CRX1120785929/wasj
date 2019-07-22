0627b31ead1dda2507b6869c2c056be7f0573ec4
wasj <1120785929@qq.com>
2019-07-22 17:40:31 +0800

    网安世纪第一次提交

---
+++ case/src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    String name;
+    int age;
+
+    public void show() {
+        String name = "晓晓";
+        System.out.println(name + "，" + age);
+        System.out.println(this.name + "，" + age);
+    }
+}
+
